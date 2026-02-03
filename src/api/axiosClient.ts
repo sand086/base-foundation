6811bfd5bbf65ccb6c2c713a81c4ef134f47ca80
Ma. Elena Sandoval <49777594+sand086@users.noreply.github.com>
2026-02-03 17:32:18 -0600

    Update axiosClient.ts

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 // Vite: variables deben empezar con VITE_
-const baseURL =
-  import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8000/api";
+const baseURL = import.meta.env.VITE_API_BASE_URL ?? "http://23.29.114.149/api";
 
 const axiosClient = axios.create({
   baseURL,
