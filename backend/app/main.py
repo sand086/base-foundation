22fdbf27c8148462c1dbb80c111c242d14f5dab3
Ma. Elena Sandoval <49777594+sand086@users.noreply.github.com>
2026-02-03 17:41:07 -0600

    asdas

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,11 +22,12 @@ origins = [
     "http://localhost:3000",
     "http://127.0.0.1:8080",
     "http://127.0.0.1:5173",
+    "http://23.29.114.149",
+    "http://23.29.114.149:80",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
