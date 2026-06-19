07194161c9fd23d4b6177117f425a1da8da6ab3c
Ma. Elena Sandoval <49777594+sand086@users.noreply.github.com>
2026-06-19 17:08:42 -0600

    asd

--- src/api/generated/models/CFDIHistoryRecord.ts
+++ src/api/generated/models/CFDIHistoryRecord.ts
@@ -10,10 +10,12 @@ export type CFDIHistoryRecord = {
     id: number;
     tipo_documento: string;
     folio?: (string | null);
+    folio_relacionado?: (string | null);
     uuid?: (string | null);
     fecha_emision?: (string | null);
     estatus: string;
     cliente_proveedor_nombre: string;
+    cliente_proveedor_rfc?: (string | null);
     monto_total?: (number | null);
     creado_por_nombre?: (string | null);
     modificado_por_nombre?: (string | null);
