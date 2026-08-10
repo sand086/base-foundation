dae93faa10a9dbe8de35e3b16d8bd0c58c4fea84
Ma. Elena Sandoval <49777594+sand086@users.noreply.github.com>
2026-08-10 17:32:49 -0600

    asd

--- backend/cancelar_serie_f.py
+++ backend/cancelar_serie_f.py
@@ -23,34 +23,9 @@ logger = logging.getLogger("cancelacion_forzada")
 UUIDS_A_CANCELAR = [
     # 1. Matar a los dos Complementos de Pago (Hijos de la 4CC84F95)
     {
-        "uuid": "7445D9F3-0FD8-4258-836D-E0F01E7A9F2A",
-        "motivo": "01",
-        "sustitucion": "65EB5C6B-0121-4C58-ADEF-473253C5B85A",
-    },
-    {
-        "uuid": "F4830BB2-C67D-49FC-846D-E5A613C2CB08",
-        "motivo": "01",
-        "sustitucion": "65EB5C6B-0121-4C58-ADEF-473253C5B85A",
-    },
-    {
-        "uuid": "F34AC46D-AF5B-48D8-A1D6-742AFEDDCA4B",
-        "motivo": "01",
-        "sustitucion": "65EB5C6B-0121-4C58-ADEF-473253C5B85A",
-    },
-    {
-        "uuid": "E5D9A238-8C00-4D31-8506-D014564C6DF1",
-        "motivo": "01",
-        "sustitucion": "900E7196-8DF8-4AEC-8197-9686C5120495",
-    },
-    {
-        "uuid": "B196EFE4-2170-40E9-BC99-B96824781121",
-        "motivo": "01",
-        "sustitucion": "900E7196-8DF8-4AEC-8197-9686C5120495",
-    },
-    {
-        "uuid": "997A4457-1E3E-401A-892F-CE64203321FB",
-        "motivo": "01",
-        "sustitucion": "900E7196-8DF8-4AEC-8197-9686C5120495",
+        "uuid": "AFD65A3C-E1E5-4438-9BD7-227B1F89AA35",
+        "motivo": "02",
+        "sustitucion": "",
     },
 ]
 
