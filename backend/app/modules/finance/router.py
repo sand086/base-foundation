7da61ed4330d102402eb5c8c602b21773d639a3d
Ma. Elena Sandoval <49777594+sand086@users.noreply.github.com>
2026-09-09 15:32:52 -0600

    asd

--- backend/app/modules/finance/router.py
+++ backend/app/modules/finance/router.py
@@ -2379,9 +2379,8 @@ def chain_cancel_trip_invoices(
     SUPERVISORES_PERMITIDOS = [
         "desarrollosoft@asicomsystems.com.mx",
         "trafico2@3t.com.mx",
-        "gerencia@3t.com.mx",
         "contabilidad@3t.com.mx",
-        "Jmartinez@3t.com.mx",
+        "jmartinez@3t.com.mx",
     ]
     CLAVE_AUTORIZACION = "Rapidos3TCancelar!"  # Clave maestra
 
